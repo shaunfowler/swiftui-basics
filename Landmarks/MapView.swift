61e780c829b552f8028ed19d5fc0ed312314ea4d
Shaun Fowler <shf@amazon.com>
2020-05-23 12:52:18 -0700

    Sample Landmarks project

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Fowler, Shaun on 5/23/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34, longitude: -116)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
